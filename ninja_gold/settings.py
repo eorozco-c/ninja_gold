b1fb02eaa27862688536c9adf7c03d32cd3b9c25
Ernesto Orozco <42546849+eorozco-c@users.noreply.github.com>
2021-07-22 12:03:18 -0400

    Update settings.py

--- ninja_gold/settings.py
+++ ninja_gold/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fnca0@4o##is7+h^70--y^09ig_1g!c!on!7sb41w!6px(1tr1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
